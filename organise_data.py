645b1178bc579aef6df7a1299d33d2e1cd74c086
Emma Lind <emmal8@kth.se>
2021-04-26 10:26:16 +0200

    addad function for seperating train,val,test

--- organise_data.py
+++ organise_data.py
@@ -2,17 +2,12 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
-#tensorflow 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
+import shutil, os
+import random
 
 
-#DATA_PATH = '/content/data/???'
 TRAIN_SPLIT = 'Dataset/TrainAndValList/train.lst'
-TEST_SPLIT = 'Dataset/TrainAndValList/validation.lst'
-#DATA = 'Dataset/Low-Resolution/200-n000008-Airedale'
+VAL_SPLIT = 'Dataset/TrainAndValList/validation.lst'
 
 # Data handling
 # FROM https://github.com/RuoyiDu/PMG-Progressive-Multi-Granularity-Training/blob/master/organize_dataset.py
@@ -31,15 +26,16 @@ with open(TRAIN_SPLIT) as fp:
         #_, _, class_name = class_name.split('/')
         
         dataFolder_train = 'data/dataset/train/' + class_name
+        dataFolder_val = 'data/dataset/val/' + class_name
         dataFolder_test = 'data/dataset/test/' + class_name
         labels.append(class_name)
         if not os.path.exists(dataFolder_train):
           os.makedirs(dataFolder_train)
-        if not os.path.exists(dataFolder_test):
-          os.makedirs(dataFolder_test) 
+        if not os.path.exists(dataFolder_val):
+          os.makedirs(dataFolder_val) 
         line = fp.readline()
 
-# Creating structure split for training data and test data
+# # Creating structure split for training data and test data
 with open(TRAIN_SPLIT) as fp:
    line = fp.readline()
    while line:
@@ -62,13 +58,11 @@ with open(TEST_SPLIT) as fp:
        _, image_id = image_id.split('n')
        image_id, _ = image_id.split('.')
        image_id.replace('n', '')
-       split[int(image_id)] = 'test'
+       split[int(image_id)] = 'val'
        tmp = line.replace('.//', '')
        line = fp.readline()
 
-# Put training data into correct directories
-# TODO
-import shutil, os
+# # Put training and validation data into correct directories
 from pathlib import Path
 with open(TRAIN_SPLIT, mode='r', encoding='utf-8-sig') as fp:
    line = fp.readline()
@@ -85,7 +79,7 @@ with open(TRAIN_SPLIT, mode='r', encoding='utf-8-sig') as fp:
        shutil.move(src, dst)
        line = fp.readline()
 
-with open(TEST_SPLIT, mode='r', encoding='utf-8-sig') as fp:
+with open(VAL_SPLIT, mode='r', encoding='utf-8-sig') as fp:
    line = fp.readline()
    while line:
        line = line.strip()
@@ -96,6 +90,25 @@ with open(TEST_SPLIT, mode='r', encoding='utf-8-sig') as fp:
        _, jpg_name = image_name_id.split('/')
    
        src = os.path.join('Dataset/Low-Resolution/', tmp)
-       dst = os.path.join('data/dataset/test', class_name, jpg_name)
+       dst = os.path.join('data/dataset/val', class_name, jpg_name)
        shutil.move(src, dst)
        line = fp.readline()
+
+# Extract a testset from the validationset 20% of the validationset
+rootdir = 'data/dataset/val'
+newdir = 'data/dataset/test'
+
+for subdir, dirs, files in os.walk(rootdir):
+   for dir in dirs:
+      source = os.path.join(rootdir, dir)
+      dest = os.path.join(newdir, dir)
+      files = os.listdir(source)
+      no_of_files = int(len(files) * 0.2)
+
+      for file_name in random.sample(files, no_of_files):
+         #print("image: ", file_name, "path: ", os.path.join(source, file_name), dest)
+         shutil.move(os.path.join(source, file_name), dest)
+
+
+
+
